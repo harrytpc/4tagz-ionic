58179f23850633fda22d473c413d3af74438a0bc
Gabriel Galhardi <gabriel.galhardi@zarpsystem.com.br>
2017-01-15 12:07:35 -0200

    Contatos

--- www/js/controllers/contacts.controller.js
+++ www/js/controllers/contacts.controller.js
@@ -1,13 +1,27 @@
 angular.module('app.controllers')
 .controller('ContactsCtrl', function($scope, $rootScope, ionicToast, BaseService) {
+	$scope.contactList = [];
+	if(!$rootScope.loggedUser){
+		$rootScope.loggedUser = {};
+		$rootScope.loggedUser.id = "312432432";
+	}
 	$scope.listContact = function(){  
 	   	BaseService.executarURLGet('/relationships/' + $rootScope.loggedUser.id)
 			.success(function (data) {
-
+				$scope.contactList = [
+					{id:"123", name:"Harry", avatar:"img/ben.png"}, 
+					{id:"123", name:"Ponte", avatar:"img/mike.png"}, 
+					{id:"123", name:"Gabriel", avatar:"img/galhardi.jpg"}, 
+					{id:"123", name:"Alan", avatar:"img/max.png"}, 
+				];
 	      	})
 	    	.error(function (error) {
 	        	ionicToast.show('Usuário inválido.', 'middle', false, 1500);
 	      	});
+   }
+
+   $scope.detail = function(id){
+	
    }
 
    $scope.listContact();
